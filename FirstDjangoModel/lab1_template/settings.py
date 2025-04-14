cd38a1813715841fef86699bbe76960373625326
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-04-14 10:43:54 +0500

    Django Installation

--- FirstDjangoModel/lab1_template/settings.py
+++ FirstDjangoModel/lab1_template/settings.py
@@ -4,7 +4,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '#Replace it with the generated password#',
+        'PASSWORD': 'nn123#nn',
         'HOST': 'postgres',
         'PORT': '5432',
     }
