62b8f9942e851f071c4a629ef0efec19d91d77f7
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-05-05 14:43:19 +0500

    Lakeside Quill - Updates

--- LakesideQuill/blog/urls.py
+++ LakesideQuill/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog')
+    path('', views.blog, name='blog'),
+    path('<int:blog_id>/', views.details, name='details'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
 ]
--- LakesideQuill/blog/views.py
+++ LakesideQuill/blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from .models import Blog
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def blog(request):
     blogs = Blog.objects.all()
     return render(request, 'blog.html', {'blogs': blogs})
+
+def details(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'details.html', {'blog': blog})
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
