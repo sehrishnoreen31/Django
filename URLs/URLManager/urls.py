8c15de7367ac04fe415772019fafd5fe0eebecea
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-05-15 11:25:30 +0500

    Deletion

--- URLs/URLManager/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('teams/', views.teams, name='teams'),
-    path('teams/<str:name>/', views.team, name='team'),
-    path('cities/', views.cities, name='cities'),
-]
