39c03c7fe504dc4bd35e7b20107ffc94f5139702
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-05-18 14:36:39 +0500

    Admin and models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    empno = models.CharField(max_length=50)
+    empname = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    salary = models.IntegerField()
+    joined_date = models.DateField(auto_now=True)
+    class Meta:
+        db_table = 'emplyee'
+
+    def __str__(self):
+        return self.empname
+    
