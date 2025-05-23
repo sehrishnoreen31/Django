63af7ebc5f2301fb15f1a686b309738e8e54860a
Sehrish Noreen <sehrishnoreen31@gmail.com>
2025-05-23 18:46:08 +0500

    Added form to add new blog

--- LakesideQuill/blog/urls.py
+++ LakesideQuill/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:blog_id>/', views.details, name='details'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('add_blog/', views.add_new_blog, name='add_blog'),
 ]
--- LakesideQuill/blog/views.py
+++ LakesideQuill/blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blog
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def blog(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.all().order_by('-date', '-id')
     return render(request, 'blog.html', {'blogs': blogs})
 
 def details(request, blog_id):
@@ -16,3 +18,15 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def add_new_blog(request):
+    if request.method == 'POST':
+        ttl = request.POST['title']
+        img = request.FILES['blog-img']
+        content = request.POST['blog-content']
+
+        new_blog = Blog(title=ttl, image=img, paragraph=content)
+        new_blog.save()
+        messages.success(request, f'Added the blog "{new_blog.title}"')
+        return redirect('add_blog')
+    return render(request, 'add-blog.html')
